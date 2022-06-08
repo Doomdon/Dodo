1cc1be3ed7d519c05cc44e3e0ccd664a0a1234e3
Doomdon <2andrusha3@gmail.com>
2022-06-08 13:58:12 +0300

    todo app

---
+++ dodo/todo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def signup(request):
+    return render(request, 'todo/signup.html')
+
+
+# def home(request):
+#     return render(request, 'todo/home.html')
